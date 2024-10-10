3a86d689290b43ad358001ed17ad47255771e99e
s29648 <s29648@pjwstk.edu.pl>
2024-10-10 16:33:33 +0200

    fix: Specified who adds `Adder` and who adds 'Subtractor' classes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 // TODO: we need to add the missing classes!
 
-// OK, I will add `Adder` and s29999 will add `Subtractor`.
+// OK, I will add `Subtractor` and s29999 will add `Adder`.
 public class Main {
     public class Main {
         public static void main(String[] args) {
